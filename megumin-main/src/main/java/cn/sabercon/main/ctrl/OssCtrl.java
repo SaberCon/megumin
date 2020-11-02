0a17b539a18568797603ac518163391d22cb0f82
SaberCon <894876785@qq.com>
2020-11-02 16:22:59 +0800

    add oss token getter

--- megumin-main/src/main/java/cn/sabercon/main/ctrl/OssCtrl.java
+++ megumin-main/src/main/java/cn/sabercon/main/ctrl/OssCtrl.java
@@ -2,6 +2,7 @@ package cn.sabercon.main.ctrl;
 
 import cn.hutool.core.util.IdUtil;
 import cn.sabercon.common.anno.ServiceController;
+import cn.sabercon.main.domain.model.OssToken;
 import cn.sabercon.main.enums.type.FileType;
 import cn.sabercon.main.manager.OssManager;
 import io.swagger.annotations.Api;
@@ -9,6 +10,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -26,12 +28,18 @@ public class OssCtrl {
     private final OssManager manager;
 
     @ApiOperation("上传文件")
-    @PostMapping("file")
+    @PostMapping
     public String upload(MultipartFile file, @NotNull FileType type, @ApiParam("为空时生成 uuid") String filename) {
         if (StringUtils.isEmpty(filename)) {
             filename = IdUtil.simpleUUID();
         }
         return manager.upload(file, type, filename);
+    }
+
+    @ApiOperation("获取临时 token")
+    @GetMapping
+    public OssToken getToken(@NotNull FileType type) {
+        return manager.getToken(type);
     }
 
 }
