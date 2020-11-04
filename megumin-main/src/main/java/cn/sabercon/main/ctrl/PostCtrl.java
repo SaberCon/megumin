dc6a79c1dc560145c1522b137d514ed0ccd742f6
SaberCon <894876785@qq.com>
2020-11-04 17:54:30 +0800

    add post ctrl

--- megumin-main/src/main/java/cn/sabercon/main/ctrl/PostCtrl.java
+++ megumin-main/src/main/java/cn/sabercon/main/ctrl/PostCtrl.java
@@ -1,9 +1,17 @@
 package cn.sabercon.main.ctrl;
 
 import cn.sabercon.common.anno.ServiceController;
+import cn.sabercon.common.domian.PageModel;
+import cn.sabercon.common.domian.PageQuery;
+import cn.sabercon.main.domain.model.PostModel;
 import cn.sabercon.main.service.PostService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 /**
  * @author SaberCon
@@ -15,4 +23,15 @@ import lombok.RequiredArgsConstructor;
 public class PostCtrl {
 
     private final PostService service;
+
+    @GetMapping("recent")
+    @ApiModelProperty("最新更新分页查询")
+    public PageModel<PostModel> listRecent(@NotNull Long communityId, @Valid PageQuery pageQuery) {
+        return service.listRecent(communityId, pageQuery);
+    }
+
+    @GetMapping
+    public PostModel get(@NotNull Long id) {
+        return service.get(id);
+    }
 }
--- megumin-main/src/main/java/cn/sabercon/main/service/PostService.java
+++ megumin-main/src/main/java/cn/sabercon/main/service/PostService.java
@@ -1,5 +1,10 @@
 package cn.sabercon.main.service;
 
+import cn.sabercon.common.domian.PageModel;
+import cn.sabercon.common.domian.PageQuery;
+import cn.sabercon.common.json.Json;
+import cn.sabercon.main.domain.entity.Post;
+import cn.sabercon.main.domain.model.PostModel;
 import cn.sabercon.main.repo.PostRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,4 +18,20 @@ import org.springframework.stereotype.Service;
 public class PostService {
 
     private final PostRepo repo;
+
+    private final UserService userService;
+
+    public PageModel<PostModel> listRecent(Long communityId, PageQuery pageQuery) {
+        return PageModel.from(repo.findByCommunityIdOrderByMtimeDesc(communityId, pageQuery.toPageRequest())).map(this::convert);
+    }
+
+    public PostModel get(Long id) {
+        return repo.findById(id).map(this::convert).orElse(null);
+    }
+
+    private PostModel convert(Post post) {
+        PostModel model = Json.convert(post, PostModel.class);
+        model.setUser(userService.getSimpleInfo(post.getUserId()));
+        return model;
+    }
 }
