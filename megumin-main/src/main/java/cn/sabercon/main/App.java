2a6f23d157247fa44167f184a2d3653d9b82dab7
SaberCon <894876785@qq.com>
2021-02-07 19:07:41 +0800

    fix bug
    Signed-off-by: SaberCon <894876785@qq.com>

--- megumin-main/src/main/java/cn/sabercon/main/App.java
+++ megumin-main/src/main/java/cn/sabercon/main/App.java
@@ -1,16 +1,33 @@
 package cn.sabercon.main;
 
+import cn.sabercon.common.data.RedisHelper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 /**
  * @author SaberCon
  * @since 1.0.0
  */
+@EnableScheduling
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class);
+    }
+
+    @Autowired
+    RedisHelper redisHelper;
+
+    /**
+     * todo redis 现在有问题, 需要不间断使用不然会断开连接, 待处理
+     */
+    @Scheduled(fixedRate = 10 * 1000)
+    public void keepRedisConnection() {
+        System.out.println("redis 保持连接中...");
+        redisHelper.set("keep-alive", 0);
     }
 }
